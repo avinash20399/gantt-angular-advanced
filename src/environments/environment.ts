5dba537d20150817d9ec23af4965d17c1186566c
Avinash Singh <87532538+avinash20399@users.noreply.github.com>
2025-05-23 12:20:06 +0530

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     //apiUrl: 'http://localhost:3000/', // Replace with your actual API URL/
-    apiUrl: 'https://quality.ktern.com'
+    apiUrl: 'http://localhost:3000/',
 };
 
 /*
